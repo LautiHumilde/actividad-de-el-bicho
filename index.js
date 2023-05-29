4858826714d748c235de1986edaa3209cb196b40
LautiHumilde <132624519+LautiHumilde@users.noreply.github.com>
2023-05-29 10:55:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const fs = require('fs');
+const port = 3000;
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const { get, Server } = require('http');
+const res = require('express/lib/response');
+
+app.set('view engine' , 'ejs')
+app.set('views', __dirname + '/views');
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+}) 
+
+app.get('/index' , (req,res) => {
+    res.render('humildad')
+})
+
+app.post('/index' , (req,res) => {
+    const Nombre = req.body.Nombre;
+    const Segundo_Nombre = req.body.Segundo_Nombre;
+    const Apellido = req.body.Apellido;
+    const Edad = req.body.Edad;
+    const datos = [Nombre,Segundo_Nombre,Apellido,Edad]
+    console.log(datos);
+}) 
